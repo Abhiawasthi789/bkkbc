89e2300857e38e2f28254fd0d5a7cbe4520ff2a8
theamit-009 <aim.amit9@gmail.com>
2019-03-10 18:13:35 +0530

    'mmcmkjbjbd'

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.post('/update', function(req, res) {
         if (err) console.log(err);
         conn.query(
             'UPDATE salesforce.Product__c SET Product_Name__c = $1,Price__c = $2 WHERE LOWER(Buyer_Name__c) = LOWER($3) AND LOWER(Buyer_Email__c) = LOWER($4) AND LOWER(Buyer_Phone__c) = LOWER($5)',
-            [req.body.productName.trim(), req.body.productPrice.trim(), req.body.buyerName.trim(), req.body.buyerEmail.trim(), req.body.buyerPhone.trim()],
+            [req.body.productName.trim(), req.body.productPrice, req.body.buyerName.trim(), req.body.buyerEmail.trim(), req.body.buyerPhone],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
                   conn.query('INSERT INTO salesforce.Product__c (Product_Name__c, Price__c, Buyer_Name__c, Buyer_Email__c, Buyer_Phone__c) VALUES ($1, $2, $3, $4, $5)',
-                  [req.body.productName.trim(), req.body.productPrice.trim(), req.body.buyerName.trim(), req.body.buyerEmail.trim(), req.body.buyerPhone.trim()],
+                  [req.body.productName.trim(), req.body.productPrice, req.body.buyerName.trim(), req.body.buyerEmail.trim(), req.body.buyerPhone],
                   function(err, result) {
                     done();
                     if (err) {
